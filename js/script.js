d33479c1ef2ac4f2a0384b512f17503cda3ebe23
Efimov-68 <r.efimov@list.ru>
2017-03-14 19:56:01 +0300

    добавил кода

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-alert=prompt('Как тебя зовут?');
-function test(name){
-	alert("Привет! "+name+'.');
+/*alert('Привет!'+' '+name);
+
+
+for (i=1; i<=5; i++){
+	document.write(i+"<br/>");
+} 
+switch (expression){
+	case n1:
+	statements
+	break;
+	case n2:
+	statements
+	break;
+	default:
+	statements
 }
-test();
+
+function myFunction(a, b){
+	return a * b;
+}
+var x = myFunction(5, 6);
+alert(x);*/
+var result = confirm('Do you really want to leave this page?');
+if(result==true){
+	alert('Thanks for visiting');
+}
+else{
+		alert('Thanks for staying with us');
+	}
+	
