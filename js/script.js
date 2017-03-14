94ba2f360d62a1b1b60142414d8634986953a902
Efimov-68 <r.efimov@list.ru>
2017-03-14 21:58:06 +0300

    изменения в js

--- js/script.js
+++ js/script.js
@@ -59,11 +59,67 @@ function test(a, b){
 	}
 }
 alert(test(5, 15));*/
+//
 var result = confirm('Do you really want to leave this page?');
 if(result==true){
 	alert('Thanks for visiting');
 }
 else{
-		alert('Thanks for staying with us');
+	alert('Thanks for staying with us');
+}
+//
+var myNum1=7;
+var myNum2=10;
+if(myNum1<myNum2){
+	alert("JavaScript is easy to leaen.");
+}
+else{
+	alert("Param pam pam")
+}
+//
+var course = prompt("Введите число от 1 до 3");
+if(course==1){
+	document.write("<h1>HTML</h1>");
+}
+else if(course == 2){
+	document.write("<h1>CSS</h1>");
+}
+else if(course == 3){
+	document.write("<h1>JS</h1>");
+}
+else{
+	document.write("NoNe");
+}
+//
+var i=0;
+for(;i<10;){
+	document.write(i+'<br/>');
+	i++;
+}
+//
+var x=1;
+while(x<=10){
+	document.write(x+"<br/>");
+	x++;
+}
+//
+var y=20;
+do{
+	document.write(y+'<br/>');
+	y++;
+}
+while(y<=25);
+//
+for(z=0; z<=10; z++){
+	if(z==5){
+		break;
 	}
-	
+	document.write(z+"<br/>");
+}
+//
+for(z=0; z<=10; z++){
+	if(z==5){
+		continue;
+	}
+	document.write(z+'<br/>');
+}
