10a830642a6d89d04e197dc6aa8cf8ebc0b8811f
Efimov-68 <r.efimov@list.ru>
2017-04-13 23:22:23 +0300

    new file

---
+++ шаблоны сайтов/web-site/The Big Bang - SITE/CONFIG.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * LordWEB
+ *
+ * @package		LordWEB
+ * @author		Vladica Savic
+ * @copyright           Copyright (c) 2011, LordWEB.
+ * @link		http://vladicasavic.iz.rs/
+ * @since		Version 1.0
+ * @filesource
+ */
+
+/*
+  | -------------------------------------------------------------------
+  | BBang CONFIG
+  | -------------------------------------------------------------------
+  | Please fill your site configation details here.
+  | CHANGE ONLY THIS FEW LINE BELLOW
+*/
+
+//SITE GLOBAL CONFIGURATION
+$email = "#"; 							//<-- Your email
+$subscribe_email = "#"; 					//<<-- Email address for sending you subscribe notification
+$subscribe_notification_subject = "New site subscriber!"; 	//<<-- Email subscribe notification subject
+
+//SOCIAL CONFIGURATION
+$twitter_name = "drythemes"; 										//<-- Twitter name you want to follow (show tweets) on page and show stats
+$facebook_statistic_page = "http://www.facebook.com/themeforest";	//<-- URL of wanted page whitch number of "LIKES" you want to get
+
+//SITE COUNTER CONFIGURATION
+$year = "2012"; 	//<<-- End date year
+$month = "10"; 		//<<-- End date month (numeric)
+$day = "21";		//<<-- End date day (numeric)
+$hour = "22";		//<<-- End date hour (numeric, less than 24 (use 00 for midnight))
+$minutes = "00";	//<<-- End date minutes	(numeric, less than 60)
+$second = "00";		//<<-- End date second (numeric, less than 60)
+?>
